6b6d3f9e668b049155e811d56708353f2f3df571
Cristiano Veiga <veiga.cristian@gmail.com>
2022-04-17 13:15:46 -0400

    about dialog improvements

--- src/components/window/common/AboutWnd/AboutWnd.js
+++ src/components/window/common/AboutWnd/AboutWnd.js
@@ -3,6 +3,7 @@ import './AboutWnd.css';
 
 const AboutWnd = ({
     title="About",
+    content="",
     visible=false,
     onOk,
     ...otherProps
@@ -19,6 +20,18 @@ const AboutWnd = ({
             <div className="rolext-about-wnd">
                 <h1>React-OlExt 0.1 Beta</h1>
                 <h2>Components for Building Web Map Applications</h2>
+                <br/>
+                <ul>
+                    <li>
+                        <label><a rel="noreferrer" href='https://mapuiexts.github.io/react-olext.github.io/en/v0.1/apidoc/' target='_blank' >API Docs</a></label>         
+                    </li>
+                    <li>
+                        <label><a rel="noreferrer" href='https://github.com/mapuiexts/React-OlExt' target='_blank' >Source Code</a></label>
+                    </li>
+                </ul>
+                <div>
+                    <p>{content}</p>
+                </div>
             </div>
       </Window>
     );
